e75da2b96f9318dd1123940accb63a038a7b76ae
Proypb20 <34356468+Proypb20@users.noreply.github.com>
2025-04-15 22:42:34 -0300

    aaa

--- src_old/main/java/com/val/gerencial/repository/GerencialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.val.gerencial.repository;
-
-import com.val.gerencial.model.Gerencial;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GerencialRepository extends JpaRepository<Gerencial, Long> {
-}
